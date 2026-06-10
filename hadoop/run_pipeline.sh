af3df7f1b3598ad64b74eca67d6abc0a4c756197
Ausath Ikram <ausathdzil@gmail.com>
2026-06-10 16:46:25 +0700

    fix: -files option

--- hadoop/run_pipeline.sh
+++ hadoop/run_pipeline.sh
@@ -5,12 +5,12 @@ hdfs dfs -rm -r -skipTrash /diabetes_project/output 2>/dev/null
 echo "Starting Hadoop Streaming Job..."
 
 mapred streaming \
+  -files hadoop/mapper.py,hadoop/reducer.py \
   -input /diabetes_project/input/diabetes_012_health_indicators_BRFSS2015.csv \
   -input /diabetes_project/input/diabetes_binary_health_indicators_BRFSS2015.csv \
   -output /diabetes_project/output \
   -mapper "python3 mapper.py" \
   -reducer "python3 reducer.py" \
-  -files hadoop/mapper.py,hadoop/reducer.py
 
 echo "Job Finished"
 
